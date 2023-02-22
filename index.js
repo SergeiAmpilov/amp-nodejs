4a38971963d5926b07860ee19f4148bfa77ef857
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-23 00:57:50 +0300

    feat: ready with node js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+
+const pagesRouter = require("./routes/pages");
+
+
+
+const app = express();
+const {PORT = 3000} =  process.env;
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+mongoose.set("strictQuery", false);
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+app.use(pagesRouter);
+
+
+async function start() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ampdb')
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`)
+    });
+  } catch (e) {
+    console.log(`Error ${e.message}`);
+  }
+}
+
+
+start();
